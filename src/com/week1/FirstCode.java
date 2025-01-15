feb2c3eb379fd7ee78147f3b731fe38c8096531f
darshan-ts <darshan.thippeswamy94@gmail.com>
2025-01-15 12:38:31 +0530

    Java Day-13

---
+++ src/com/week1/FirstCode.java
@@ -0,0 +1,14 @@
+package com.week1;
+
+public class FirstCode {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("darshan");
+		
+		System.out.println("Welcome to learn Java programming");
+		
+		System.out.println("Welcome to learn Selenium");
+	}
+	
+}

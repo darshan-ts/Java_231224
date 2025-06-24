32427221b126d359ab8489b66b9684f58a8e9fb1
darshan-ts <darshan.thippeswamy94@gmail.com>
2025-06-24 15:40:51 +0530

    24-06-2025_Updated Notes and pom.xml

---
+++ src/test/java/com/AppTesting/DriverSession_1/FirstAppiumSessionUsingDesiredCapabilities.java
@@ -0,0 +1,16 @@
+package com.AppTesting.DriverSession_1;
+
+import org.testng.annotations.Test;
+
+public class FirstAppiumSessionUsingDesiredCapabilities {
+  @Test
+  public void testAppiumDriverSession() {
+	  //create Appium driver capabilities
+	  
+	  
+	  //server address where we can send these capabilities
+	  
+	  
+	  //create a 	driver session
+  }
+}
